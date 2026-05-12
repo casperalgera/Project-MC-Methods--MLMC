072e5d4489237195dd6a6ad4bd758eecc2c701cc
Casper <55704256+casperalgera@users.noreply.github.com>
2026-05-12 18:15:57 +0200

    Fixed the root-finding for omega_0

--- MLMC.py
+++ MLMC.py
@@ -4,15 +4,17 @@ import matplotlib.pyplot as plt
 from scipy.optimize import brentq
 
 sigmasq = 1 #>= 1
-lamb = 0.6   #<= Diam(D) = 1
+lamb = 1   #<= Diam(D) = 1
 cov = lambda x,y: sigmasq * np.exp(-abs(x-y)/lamb) 
 
 
 #We find the nth solution of omega using the periodicity of tan combined with the fact that 1/x**2 -> 0
+#Note that the function has been rewritten into a form more suitable for numerical root-finding
 #TODO: Turn into precomputed array to speed up calculations.
-omega_0 = brentq(lambda x: np.tan(x) - (2*lamb*x)/(lamb**2*x**2 - 1), 1.2*(1.7-lamb + np.log10(lamb+1)), 1.9*(1.7-lamb + np.log10(lamb+1)), xtol=9e-15, rtol=1e-15)
-omega = lambda n: omega_0 if n ==0 else brentq(lambda x: np.tan(x) - (2*lamb*x)/(lamb**2*x**2 - 1), n*np.pi - np.pi/4, n*np.pi + np.pi/2.5)
-print(omega(0), omega(1), omega(2), omega(3))
+f = lambda x: (lamb**2*x**2 - 1)*np.sin(x) - 2*lamb*x*np.cos(x)
+omega_0 = brentq(f, 0.01, np.pi - np.pi/4)
+omega = lambda n: omega_0 if n == 0 else brentq(f, n*np.pi - np.pi/4, n*np.pi + np.pi/2.5)
+
 theta_1D = lambda n: (2*lamb) / (lamb**2 * omega(n)**2 + 1)
 
 def b_1D(n, x):
@@ -23,17 +25,16 @@ def b_1D(n, x):
 def Truncated_KL_Expansion(x, theta, b):
     theta_sum = 0
     m_KL = 0
-    while theta_sum < 4:
+    while theta_sum < 0.99:
         theta_sum += theta(m_KL)
         print(theta_sum)
         m_KL += 1
     print(m_KL)
     return np.sum([np.sqrt(theta(i))*np.random.normal(0,1)*b(i, x) for i in range(m_KL)])
 
-
-# plt.hist([Truncated_KL_Expansion(-5, theta_1D, b_1D) for _ in range(1000)], alpha=0.5)
-# plt.hist([Truncated_KL_Expansion(5, theta_1D, b_1D) for _ in range(1000)], alpha=0.5)
-# plt.show()
+plt.hist([Truncated_KL_Expansion(-5, theta_1D, b_1D) for _ in range(10)], alpha=0.5)
+plt.hist([Truncated_KL_Expansion(5, theta_1D, b_1D) for _ in range(10)], alpha=0.5)
+plt.show()
 
 
 #Sampel from random input filed k(x, \omega)
